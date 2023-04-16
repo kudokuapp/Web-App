86fbcc7960276c836f1a2000c5cb1e2461eb80e5
Furqon Wilogo <fdwilogo@gmail.com>
2023-04-16 21:30:57 +0700

    update toast class

--- components/ToastFC/index.tsx
+++ components/ToastFC/index.tsx
@@ -8,9 +8,9 @@ const ToastFC: React.FC = () => {
   const { isDesktop } = useContext(DeviceContext);
 
   if (isDesktop) {
-    return <Toaster position="bottom-right" />;
+    return <Toaster position="bottom-right" containerClassName="z-[9999999]" />;
   } else {
-    return <Toaster position="top-center" />;
+    return <Toaster position="top-center" containerClassName="z-[9999999]" />;
   }
 };
 
